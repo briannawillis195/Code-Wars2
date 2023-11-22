bb66d1e509265d4ceafb99a749e03db230543c67
brianna willis <brianna.willis195@gmail.com>
2023-11-22 10:46:30 -0500

    daily commit

---
+++ fixMe.js
@@ -0,0 +1,7 @@
+//The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+//But it's not working properly.
+
+var replaceDots = function(str) {
+  return str.replace(/\./g, '-');
+}
