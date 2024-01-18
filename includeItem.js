2408cde36df9d81c462e3f174653d6ae208a57c3
brianna willis <brianna.willis195@gmail.com>
2024-01-18 17:38:07 -0500

    daily commit

---
+++ includeItem.js
@@ -0,0 +1,11 @@
+//Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+function include(arr, item){
+    // ...
+    if(arr.includes(item) === true){
+      return true
+    }else{
+      return false
+    }
+    
+  }
