6a4674c91f78b0467eeb32b23f04cd5900431b4d
brianna willis <brianna.willis195@gmail.com>
2024-01-26 13:20:32 -0500

    daily commit

---
+++ stringAddition.js
@@ -0,0 +1,7 @@
+//ES6 has introduced another way of joining strings. Your task is to find out what this is and write a function that will add two strings together, there must be a space between the two strings.
+
+function joinStrings(string1, string2){
+
+  const stringTogether = `${string1} ${string2}`
+  return stringTogether
+}
